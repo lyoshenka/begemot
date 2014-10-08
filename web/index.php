7131bc7554ad977c9baa611e33695454fbbb9623
Alex Grintsvayg <lyoshenka@gmail.com>
2014-10-08 11:42:39 -0400

    create post

--- web/index.php
+++ web/index.php
@@ -12,14 +12,31 @@ $app->get('/', function() use($app) {
   return new Response('Begemot');
 });
 
-$app->match('/mandrill_hook_endpoint', function(Request $request) use($app) { 
+function post($from, $subject, $text) {
+  $filename = date('Y-m-d') . '-' . preg_replace('/[^a-z0-9]+/', '-', strtolower($subject) . '.md');
+  file_put_contents(__DIR__.'/../posts/'.$filename, $text);
+}
+
+$app->match('/mandrill_hook_endpoint', function(Request $request) use($app) {
   if ($request->getMethod() == 'HEAD') {
     return new Response('ok');
   }
 
   $data = $request->get('mandrill_events');
+  if (!$data)
+  {
+    return new Response('no data');
+  }
+
   $filename = date('Y-m-d_H:i:s').'.json';
   file_put_contents(__DIR__.'/../requests/'.$filename, $data);
+
+  $events = json_decode($data, true);
+  foreach($events as $event)
+  {
+    post($event['from_email'], $event['subject'], $event['text']);
+  }
+
   return new Response('ok');
 })->method('POST|HEAD'); 
 
