0bda30cd163d75ee0be1c13fc421a57a682cbaee
Alex Grintsvayg <lyoshenka@gmail.com>
2015-01-19 13:04:27 -0500

    enabled analytics on some links

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,14 @@
-// When the DOM is ready,
 $(function() {
-    // Do stuff;
+  if (typeof(ga) === typeof(Function)) // analytics is enabled
+  {
+    $('body').on('click', '[data-ga]', function() {
+      var data = $(this).data('ga');
+      ga('send', {
+        hitType: 'event',                 // Required.
+        eventCategory: data.category,     // Required.
+        eventAction: data.action,         // Required.
+        eventLabel: data.label || 'none'
+      });
+    });
+  }
 });
