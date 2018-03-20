63537f0af06936daaaab004fbc6552c0caa061c2
Alex Grintsvayg <git@grin.io>
2018-03-20 15:57:04 -0400

    typo

--- app/routes/main.php
+++ app/routes/main.php
@@ -136,7 +136,7 @@ function initMainRoutes($app) {
 
     if (preg_match('/^(\r\n|\r|\n)*[\t\f\v ]*---/', $postText))
     {
-      $parts = preg_split("/(\r\n|\r|\n)[\t\f\v ]*---[\t\f\v ]*(\r\n|\r|\n)/", "\n".str_replace("---".hex2bin('e2808b')."\n","---\n",$testing), 3); //e2808b = zero-width space
+      $parts = preg_split("/(\r\n|\r|\n)[\t\f\v ]*---[\t\f\v ]*(\r\n|\r|\n)/", "\n".str_replace("---".hex2bin('e2808b')."\n","---\n",$postText), 3); //e2808b = zero-width space
 
       if (count($parts) == 3)
       {
